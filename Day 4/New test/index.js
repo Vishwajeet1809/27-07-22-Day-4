4564b7da07447e60d3b63caab21eda417a8bbd01
Vishwajeet Singh Chundawat <104882494+Vishwajeet1809@users.noreply.github.com>
2022-07-28 15:52:06 +0530

    Add files via upload

---
+++ Day 4/New test/index.js
@@ -0,0 +1,11 @@
+document.write("<br/>"+Number("3.14787868"));
+document.write("<br/>"+Number(" ")); //The spce is zero
+document.write("<br/>"+Number("")); //The null is zero
+document.write("<br/>"+Number("99 88")); //The space is null
+
+let num = 111;
+let s=String(100+23);
+document.write("<br/>"+String(num));
+document.write("<br/>"+String(123));
+document.write("<br/>"+String(s+100+23));
+document.write("<br/>"+String("100"+"23")+String("100"+"23"));
